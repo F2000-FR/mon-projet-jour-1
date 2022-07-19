cbbf5b6952078f7c3304a77b9acde9c231269ab4
F. LOUIS <contact@F2000.fr>
2022-07-19 11:37:17 +0200

    blog

---
+++ blog.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Mon site LOCAL</title>
+	</head>
+	<body>
+		<h1>Mon super site</h1>
+	        <p>Lorem epsum GITHUB</p>
+		<p>Lorem epsum LOCAL</p>
+		<form>
+            <label>
+                Mon identifiant
+                <input type="text" />
+            </label>
+        </form>
+	</body>
+</html>
