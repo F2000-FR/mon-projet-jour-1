672187c26bb50c04f451eba9325b827f97cc5c05
F. LOUIS <contact@F2000.fr>
2022-07-19 10:50:17 +0200

    Modif form

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
 	</head>
 	<body>
 		<h1>Mon super site</h1>
-        <p>Lorem epsum GITHUB</p>
+	        <p>Lorem epsum GITHUB</p>
 		<p>Lorem epsum LOCAL</p>
+		<form>
+            <label>
+                Mon identifiant
+                <input type="text" />
+            </label>
+        </form>
 	</body>
 </html>
